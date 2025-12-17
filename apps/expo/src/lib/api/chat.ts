3e803b07e5239765276dc04d6729038e41d24a3d
Taewoong Heo <chamjoeun0111@gmail.com>
2025-12-17 14:32:34 +0900

    fix(chat): sendMessage response 타입 오류

--- apps/expo/src/lib/api/chat.ts
+++ apps/expo/src/lib/api/chat.ts
@@ -1,9 +1,16 @@
+import { Chat } from '@src/features/chat';
+
 import { apiClient } from './client';
 
+interface SendMessageResponse {
+  messages: Chat[];
+}
+
 export const userAPI = {
-  sendMessage: async (message: string) => {
-    console.log('message: ', message);
-    const response = await apiClient.post('/chats', { message });
+  sendMessage: async (message: string): Promise<Chat[]> => {
+    const response = (await apiClient.post('/chats', {
+      message,
+    })) as SendMessageResponse;
     return response.messages;
   },
 };
