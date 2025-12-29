d10acabe3ffd8faeb42fd5a1c78d3406d5017e18
Taewoong Heo <chamjoeun0111@gmail.com>
2025-12-29 13:17:45 +0900

    feat(typography): 폰트 사이즈 변경

--- packages/tailwind-design-tokens/typography/index.js
+++ packages/tailwind-design-tokens/typography/index.js
@@ -1,13 +1,13 @@
 module.exports = {
-    'title-large': ['24px', { lineHeight: '32px', fontWeight: '700' }],
-    'title-medium': ['22px', { lineHeight: '30px', fontWeight: '700' }],
-    'title-small': ['20px', { lineHeight: '28px', fontWeight: '600' }],
+    'title-large': ['30px', { lineHeight: '38px', fontWeight: '700' }],
+    'title-medium': ['28px', { lineHeight: '36px', fontWeight: '700' }],
+    'title-small': ['26px', { lineHeight: '34px', fontWeight: '600' }],
   
-    'body-large': ['18px', { lineHeight: '26px', fontWeight: '400' }],
-    'body-medium': ['16px', { lineHeight: '24px', fontWeight: '400' }],
-    'body-small': ['14px', { lineHeight: '20px', fontWeight: '400' }],
+    'body-large': ['24px', { lineHeight: '32px', fontWeight: '400' }],
+    'body-medium': ['22px', { lineHeight: '30px', fontWeight: '400' }],
+    'body-small': ['20px', { lineHeight: '26px', fontWeight: '400' }],
   
-    'label-large': ['15px', { lineHeight: '22px', fontWeight: '600' }],
-    'label-medium': ['13px', { lineHeight: '18px', fontWeight: '500' }],
-    'label-small': ['11px', { lineHeight: '14px', fontWeight: '500' }],
+    'label-large': ['21px', { lineHeight: '28px', fontWeight: '600' }],
+    'label-medium': ['19px', { lineHeight: '24px', fontWeight: '500' }],
+    'label-small': ['17px', { lineHeight: '20px', fontWeight: '500' }],
 };
