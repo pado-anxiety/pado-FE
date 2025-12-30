8c7e081efe683f6c1a508b972fadf0353988bb34
Taewoong Heo <chamjoeun0111@gmail.com>
2025-12-31 02:09:56 +0900

    feat(diary): textarea 크기만큼 텍스트를 입력받도록 수정

--- apps/web/src/features/diary/components/AnswerTextarea.tsx
+++ apps/web/src/features/diary/components/AnswerTextarea.tsx
@@ -1,15 +1,32 @@
-import { RefObject } from 'react';
+import { ChangeEvent, RefObject, useCallback, useRef } from 'react';
 
 type AnswerTextareaProps = {
   textareaRef: RefObject<HTMLTextAreaElement | null>;
 };
 
 export function AnswerTextarea({ textareaRef }: AnswerTextareaProps) {
+  const prevValueRef = useRef<string>('');
+
+  const handleChange = useCallback(
+    (e: ChangeEvent<HTMLTextAreaElement>) => {
+      const target = e.target;
+
+      if (target.scrollHeight > target.clientHeight && textareaRef.current) {
+        textareaRef.current.value = prevValueRef.current;
+        return;
+      }
+
+      prevValueRef.current = target.value;
+    },
+    [textareaRef],
+  );
+
   return (
     <div className="flex-1 flex">
       <textarea
-        className="flex-1 p-2 rounded-lg border border-gray-300"
+        className="flex-1 p-2 rounded-lg border border-gray-300 resize-none overflow-hidden focus:outline-none focus:ring-0"
         ref={textareaRef}
+        onChange={handleChange}
       />
     </div>
   );
