20ce9deb3263136aec491336a363ee1dd3813ab8
Taewoong Heo <chamjoeun0111@gmail.com>
2026-01-06 13:17:44 +0900

    fix(Wave): 수평선과 바다아래부분의 레이아웃이 맞지 않는 문제

--- apps/expo/src/features/home/constants/layout.ts
+++ apps/expo/src/features/home/constants/layout.ts
@@ -2,7 +2,7 @@ import { scale } from 'react-native-size-matters';
 
 export const WAVE_LAYOUT = {
   SKY_HEIGHT: scale(300),
-  HORIZON_HEIGHT: scale(250),
+  HORIZON_HEIGHT: scale(300),
 } as const;
 
 export const FOREGROUND = {
@@ -15,27 +15,27 @@ export const FOREGROUND = {
 export const FOREGROUND_MID = {
   SPEED_MULTIPLIER: 0.5,
   FREQUENCY: 2,
-  AMPLITUDE: scale(13),
-  OFFSET: -scale(40),
+  AMPLITUDE: scale(15),
+  OFFSET: -scale(25),
 } as const;
 
 export const MIDGROUND = {
   SPEED_MULTIPLIER: 0.8,
   FREQUENCY: 2,
-  AMPLITUDE: scale(11),
-  OFFSET: -scale(70),
+  AMPLITUDE: scale(15),
+  OFFSET: -scale(50),
 } as const;
 
 export const MIDGROUND_BACK = {
   SPEED_MULTIPLIER: 0.3,
   FREQUENCY: 2,
-  AMPLITUDE: scale(8),
-  OFFSET: -scale(95),
+  AMPLITUDE: scale(15),
+  OFFSET: -scale(75),
 } as const;
 
 export const BACKGROUND = {
   SPEED_MULTIPLIER: 0.7,
   FREQUENCY: 2,
-  AMPLITUDE: scale(4),
-  OFFSET: -scale(115),
+  AMPLITUDE: scale(15),
+  OFFSET: -scale(100),
 } as const;
