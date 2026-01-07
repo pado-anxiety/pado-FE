4b24748c6ac156671b3eb4f084ba7c196a8e7e8b
Taewoong Heo <chamjoeun0111@gmail.com>
2026-01-07 22:26:19 +0900

    feat(home): 파도 높이 변경

--- apps/expo/src/features/home/constants/layout.ts
+++ apps/expo/src/features/home/constants/layout.ts
@@ -2,40 +2,40 @@ import { scale } from 'react-native-size-matters';
 
 export const WAVE_LAYOUT = {
   SKY_HEIGHT: scale(300),
-  HORIZON_HEIGHT: scale(300),
+  HORIZON_HEIGHT: scale(350),
 } as const;
 
 export const FOREGROUND = {
   SPEED_MULTIPLIER: 1,
   FREQUENCY: 2,
-  AMPLITUDE: scale(15),
+  AMPLITUDE: scale(18),
   OFFSET: 0,
 } as const;
 
 export const FOREGROUND_MID = {
   SPEED_MULTIPLIER: 0.5,
   FREQUENCY: 2,
-  AMPLITUDE: scale(15),
-  OFFSET: -scale(25),
+  AMPLITUDE: scale(18),
+  OFFSET: -scale(40),
 } as const;
 
 export const MIDGROUND = {
   SPEED_MULTIPLIER: 0.8,
   FREQUENCY: 2,
-  AMPLITUDE: scale(15),
-  OFFSET: -scale(50),
+  AMPLITUDE: scale(18),
+  OFFSET: -scale(70),
 } as const;
 
 export const MIDGROUND_BACK = {
   SPEED_MULTIPLIER: 0.3,
   FREQUENCY: 2,
-  AMPLITUDE: scale(15),
-  OFFSET: -scale(75),
+  AMPLITUDE: scale(18),
+  OFFSET: -scale(100),
 } as const;
 
 export const BACKGROUND = {
   SPEED_MULTIPLIER: 0.7,
   FREQUENCY: 2,
-  AMPLITUDE: scale(15),
-  OFFSET: -scale(100),
+  AMPLITUDE: scale(18),
+  OFFSET: -scale(130),
 } as const;
